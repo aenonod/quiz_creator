1d0128e65f35b41a08650604087b784dd4fc4549
aenonod <abegail.nonod13@gmail.com>
2025-04-09 23:57:27 +0800

    feat: add options to name and open a quiz file

--- quiz_creator_program.py
+++ quiz_creator_program.py
@@ -1,11 +1,17 @@
 # General flow of quiz creator program:
+# Ask for the filename (if user wants to create another quiz)
 # Ask user for input
 # Store data into text file
 # Loop the program until user chooses to exit
 
 
-def quiz_creator():
-    with open("quiz.txt", "a") as file:
+def file_naming():
+    filename = input("Input your filename (w/o extension): ").strip()
+    if not filename.endswith(".txt"):
+        filename += ".txt"
+
+def quiz_creator(filename):
+    with open(filename, "a") as file:
         # Input question
         print(">>> QUIZ CREATOR <<<")
         question = input(f"Input question: ")
@@ -42,15 +48,30 @@ def quiz_creator():
     file.close()
 
 # Ask another input until user chooses to exit
-while True:
-    quiz_creator()
-        
-    add_input = input("\nDo you want to add more questions? (yes/no): ").lower()
-    if add_input == "yes":
-        print()
-        continue
-    else:
-        with open("quiz.txt", "r") as file:
-            print("\n>>> FINAL QUIZ CONTENT <<<")
-            print(file.read())
-        break
+def program_main_loop():
+    while True:
+            file_naming()
+            quiz_creator()
+            
+            add_input = input("\nDo you want to add more questions? (yes/no): ").lower()
+            if add_input == "yes":
+                print()
+                continue
+            else:
+                num_1 = print("-Press 1 to create new quiz file-")
+                num_2 = print("-Press 2 to open a quiz file-")
+                num_3 = print("-Press 3 to exit-")
+
+            choice = input("Enter here: ")
+            if choice == 1:
+                program_main_loop()
+            elif choice == 2:
+                filename = input("Enter filename to open (with extension): ")
+                with open(filename, "r") as file:
+                    print(f"\n>>> QUIZ ({filename}) <<<")
+                    print(file.read())
+            elif choice == 3:
+                print("Goodbye, user!👋")
+            else:
+                print("Invalid input. Try again.")
+program_main_loop()
