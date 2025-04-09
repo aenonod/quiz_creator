1319f6839fc155c1d93443be80b7edc860970ffc
aenonod <abegail.nonod13@gmail.com>
2025-04-09 16:32:09 +0800

    feat: changes with input display and format of stored data

--- quiz_creator_program.py
+++ quiz_creator_program.py
@@ -1,24 +1,37 @@
 # Ask for user input
 # Store data into text file
 def quiz_creator():
-    with open("Quiz.txt", "w") as file:
+    with open("quiz.txt", "w") as file:
         # Input question
+        print("=========== QUIZ CREATOR ==========")
         question = input("Input a question: ")
-        file.write(question)
+        file.write(f"\nQuestion: {question}")
 
         # Input possible answers
-        choices_a = input("Possible answer: ")
-        file.write(choices_a)
-        choices_b = input("Possible answer: ")
-        file.write(choices_b)
-        choices_c = input("Possible answer: ")
-        file.write(choices_c)
-        choices_d = input("Possible answer: ")
-        file.write(choices_d)
+        print("\nNote: No need to put letters in the choices. Let the quiz creator run it's magic!✨")
+        choices_a = input("Possible answer: ").lower()
+        file.write(f"\na) {choices_a}")
+        choices_b = input("Possible answer: ").lower()
+        file.write(f"\nb) {choices_b}")
+        choices_c = input("Possible answer: ").lower()
+        file.write(f"\nc) {choices_c}")
+        choices_d = input("Possible answer: ").lower()
+        file.write(f"\nd) {choices_d}")
             
         # Input correct answer
-        correct_ans = input("Correct answer: ")
-        file.write(correct_ans)
+        correct_ans = input(f"\nCorrect answer: ").lower()
+
+        letter = ""
+        if correct_ans == choices_a:
+            letter = 'a)'
+        elif correct_ans == choices_b:
+            letter = 'b)'
+        elif correct_ans == choices_c:
+            letter = 'c)'
+        elif correct_ans == choices_d:
+            letter = 'd)'
+
+        file.write(f"\nCorrect answer: {letter} {correct_ans}")
 
     file.close()
 
@@ -26,4 +39,6 @@ def quiz_creator():
     print(file.read())
     file.close()
 
-# Ask another input until user chooses to exit
+quiz_creator()
+
+### Ask another input until user chooses to exit
