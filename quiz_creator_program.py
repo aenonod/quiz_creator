2a5a7ebc6f2c7cb362e7284aece56288c9c1a3b8
aenonod <abegail.nonod13@gmail.com>
2025-04-09 14:45:38 +0800

    add initial code to ask user for inputs

--- quiz_creator_program.py
+++ quiz_creator_program.py
@@ -1,6 +1,15 @@
-# Ask for user input 
+# Ask for user input
 # Input question
+question = input("Input a question: ")
+
 # Input possible answers
+choices_a = input("Possible answer: a. ")
+choices_b = input("Possible answer: b. ")
+choices_c = input("Possible answer: c. ")
+choices_d = input("Possible answer: d. ")
+
 # Input correct answer
+correct_ans = input("Correct answer (format: letter then desc): ")
+
 # Store data into text file
 # Ask another input until user chooses to exit
